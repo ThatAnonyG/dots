be7ea2e0a71cffbe344b94fac4a4e62b48d5bac8
Ratul Saha <ratulsaha90.rs@gmail.com>
2023-04-30 23:59:44 +0530

    fix: nvim startup dir stuff

--- nvim/lua/cfg/options.lua
+++ nvim/lua/cfg/options.lua
@@ -39,7 +39,9 @@ vim.g.copilot_no_tab_map = true -- disable tab mapping for copilot
 local os = require("os")
 local path_to_desktop = os.getenv("USERPROFILE")
 local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
-vim.api.nvim_create_autocmd(
-	{ "VimEnter" },
-	{ pattern = "*", command = "cd " .. path_to_desktop, group = vim_enter_group }
-)
+if not path_to_desktop == nil then
+	vim.api.nvim_create_autocmd(
+		{ "VimEnter" },
+		{ pattern = "*", command = "cd " .. path_to_desktop, group = vim_enter_group }
+	)
+end
