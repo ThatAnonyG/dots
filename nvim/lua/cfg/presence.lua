cc63b4dd1a7415a34ce4e8de2306f971794159ce
Ratul Saha <contact@ratulsaha.me>
2023-06-03 06:03:41 +0530

    fix: dont use log level debug ffs

--- nvim/lua/cfg/presence.lua
+++ nvim/lua/cfg/presence.lua
@@ -9,7 +9,7 @@ presence.setup({
 	neovim_image_text = "x10 Dev Stuff",
 	main_image = "neovim",
 	client_id = "793271441293967371",
-	log_level = "debug",
+	log_level = nil, 
 	debounce_timeout = 10,
 	enable_line_number = false,
 	blacklist = {},
