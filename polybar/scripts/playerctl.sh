2884445158c6aac3dde8c00fd2b08580fa75e436
Ratul Saha <contact@ratulsaha.me>
2023-06-27 22:31:20 +0530

    feat(rice/notifs): added dunst notifications and some player improvements

--- polybar/scripts/playerctl.sh
+++ polybar/scripts/playerctl.sh
@@ -2,33 +2,41 @@
 
 base_color="%{F#f5e0dc}"
 
+pctl() {
+  playerctl -p spotify $1
+}
+
+pctlcmd() {
+  echo "playerctl -p spotify $1"
+}
+
 previous_icon=
 
 play_pause_icon=
 
-if [[ $(playerctl status) = "Paused" ]]; then
+if [[ $(pctl "status") = "Paused" ]]; then
   play_pause_icon=
 fi
 
 next_icon=
 
 loop_icon="%{F#f38ba8}$base_color"
-loop_command="playerctl loop playlist"
+loop_command=`pctlcmd "loop playlist"`
 
-if [[ $(playerctl loop) = "Playlist" ]]; then
+if [[ $(pctl "loop") = "Playlist" ]]; then
   loop_icon=
-  loop_command="playerctl loop track"
-elif [[ $(playerctl loop) = "Track" ]]; then
+  loop_command=`pctlcmd "loop track"`
+elif [[ $(pctl "loop") = "Track" ]]; then
   loop_icon="%{F#a6e3a1}$base_color"
-  loop_command="playerctl loop none"
+  loop_command=`pctlcmd "loop none"`
 fi
 
 shuffle_icon="%{F#f38ba8}"
-shuffle_command="playerctl shuffle on"
+shuffle_command=`pctlcmd "shuffle on"`
 
-if [[ $(playerctl shuffle) = "On" ]]; then
+if [[ $(pctl "shuffle") = "On" ]]; then
   shuffle_icon=
-  shuffle_command="playerctl shuffle off"
+  shuffle_command=`pctlcmd "shuffle off"`
 fi
 
-echo "%{A1:playerctl previous:}$previous_icon%{A}  %{A1:playerctl play-pause:}$play_pause_icon%{A}  %{A1:playerctl next:}$next_icon%{A}  %{A1:$loop_command:}$loop_icon%{A}  %{A1:$shuffle_command:}$shuffle_icon%{A}"
+echo "%{A1:`pctlcmd "previous"`:}$previous_icon%{A}  %{A1:`pctlcmd "play-pause"`:}$play_pause_icon%{A}  %{A1:`pctlcmd "next"`:}$next_icon%{A}  %{A1:$loop_command:}$loop_icon%{A}  %{A1:$shuffle_command:}$shuffle_icon%{A}"
