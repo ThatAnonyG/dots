d6ae9f68096c34983169eae155846ce674766a83
Ratul Saha <contact@ratulsaha.me>
2023-12-02 04:46:58 +0530

    feat(polybar): added more modules and folder restructuring

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,13 @@ polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar-top.log
 
+# Set hwmon path
+for i in /sys/class/hwmon/hwmon*/temp*_input; do
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "nct6798: CPUTIN" ]; then
+        export CPU_TEMP_HWMON="$i"
+    fi
+done
+
 # Launch
 polybar top 2>&1 | tee -a /tmp/polybar-top.log & disown
 polybar bottom 2>&1 | tee -a /tmp/polybar-bottom.log & disown
