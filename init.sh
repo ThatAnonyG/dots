c371f9d1cc019280dbde9e93015c31037ab6364c
Ratul Saha <contact@ratulsaha.me>
2023-12-05 15:43:23 +0530

    todo(init): started some work on the init script

--- init.sh
+++ init.sh
@@ -1,6 +1,32 @@
-# Perform system updates first
-sudo apt-get -y update
-sudo apt-get -y upgrade
+echo "Welcome! Initializing system..."
 
-# Install all packages from APT first
-sudo apt-get -y install git
+install_package() {
+  echo "Installing $1..."
+  if [ -f "/etc/arch-release" ]; then
+    sudo pacman -S $1
+  fi
+}
+
+install() {
+  if [ -f "/etc/arch-release" ]; then
+    sudo pacman -Syu
+  fi
+  install_package "git"
+  install_package "zsh"
+}
+
+flag=$1
+
+if [ "$flag" == "-i" ]; then
+  echo "Installing..."
+  install()
+elif [ "$flag" == "-u" ]; then
+  echo "Updating..."
+else
+  echo "Usage: ./init.sh [OPTION]"
+  echo "OPTION:"
+  echo "  -h: help"
+  echo "  -i: install"
+  echo "  -u: update"
+  exit 0
+fi
